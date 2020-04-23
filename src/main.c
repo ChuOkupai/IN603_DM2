0702c1f13ece8771d98aee4677073c71e2ac657d
Adrien Soursou <asoursou@student.42.fr>
2020-04-23 14:06:23 +0200

    fix

--- src/main.c
+++ src/main.c
@@ -5,12 +5,12 @@ int	main(int ac, char **av)
 	(void)ac;
 	(void)av;
 	mpz_t r, a, h, n;
-	mpz_inits(r, a, h, n);
+	mpz_inits(r, a, h, n, NULL);
 	mpz_add_ui(a, a, 2);
 	mpz_add_ui(h, h, 26);
-	mpz_add_ui(n, n, 2);
-	//square_and_multiply(r, a, h, n);
+	mpz_add_ui(n, n, 10000000000);
+	square_and_multiply(r, a, h, n);
 	gmp_printf("r = %Zd\n", r);
-	mpz_clears(r, a, h, n);
+	mpz_clears(r, a, h, n, NULL);
 	return (0);
 }
--- src/primtest.c
+++ src/primtest.c
@@ -2,23 +2,21 @@
 
 void	square_and_multiply(mpz_t r, mpz_t a, mpz_t h, mpz_t n)
 {
-	if (mpz_cmp_ui(h, 0))
+	mp_bitcnt_t i = mpz_sizeinbase(h, 2) - 1;
+	if (!i)
 	{
 		mpz_set_ui(r, 1);
 		return ;
 	}
-	mp_bitcnt_t i = mpz_sizeinbase(h, 2);
 	mpz_add_ui(r, a, 0);
-	while (1)
+	while (i--)
 	{
 		mpz_mul(r, r, r);
 		mpz_mod(r, r, n);
-		if (mpz_tstbit(h, --i))
+		if (mpz_tstbit(h, i))
 		{
 			mpz_mul(r, r, a);
 			mpz_mod(r, r, n);
 		}
-		if (!i)
-			break ;
 	}
 }
